e6176dc9bd80d3f0af912f4a3f82df09755b05c9
scls19fr <scls19fr@users.noreply.github.com>
2019-01-24 16:04:25 +0100

    Update 1_on_off.ino

--- examples/1_on_off/1_on_off.ino
+++ examples/1_on_off/1_on_off.ino
@@ -3,14 +3,14 @@
 
 // pinout
 // connect a push button from this pin (7) to ground (GND)
-const byte PIN_BUTTON = 7;
+const byte PIN_BUTTON_0 = 7;
 // connect a led from this pin (13) to ground (GND) using a 220 ohm resistor
-const byte PIN_LED = 13;
+const byte PIN_LED_0 = 13;
 
 // instantiate a push button object with pin number PIN_BUTTON
-PushButton butToggle(PIN_BUTTON);
+PushButton butToggle(PIN_BUTTON_0);
 // instantiate a led object with pin number PIN_LED
-Led led(PIN_LED);
+Led led(PIN_LED_0);
 
 uint32_t ms = 0;
 
